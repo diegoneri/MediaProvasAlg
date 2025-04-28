be41f7ae73cbe8819cd0fd5dd1d2b7a46bb41ae7
Diego Neri Felix <Diego Neri>
2025-04-28 10:37:33 -0300

    Template para ALG

--- Program.cs
+++ Program.cs
@@ -1,18 +1,38 @@
-﻿using System.Diagnostics;
-
-Console.Clear();
-double notaUm, notaDois, media;
+﻿Console.Clear();
+double P1, P2, P3, L1, L2, L3, L4, L5, L6, media;
 const double MEDIA_APROVACAO = 6.0;
 
+Console.Write("Informe a nota da Lista 01:");
+L1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a nota da Lista 02:");
+L2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a nota da Lista 03:");
+L3 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a nota da Lista 04:");
+L4 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a nota da Lista 05:");
+L5 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe a nota da Lista 06:");
+L6 = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Informe a nota da P1:");
-notaUm = Convert.ToDouble(Console.ReadLine());
+P1 = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Informe a nota da P2:");
-notaDois = Convert.ToDouble(Console.ReadLine());
+P2 = Convert.ToDouble(Console.ReadLine());
 
-media = (notaUm + notaDois) / 2;
 
-Console.WriteLine($"Média: {media:N1}");
+Console.Write("Informe a nota da P2:");
+P3 = Convert.ToDouble(Console.ReadLine());
+
+media = 0;
+
+//Console.WriteLine($"Média: {media:N1}");
 
 bool aprovado = media >= MEDIA_APROVACAO;
 
