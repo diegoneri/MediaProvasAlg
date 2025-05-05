4b3f9a10093914127d4802a11c2676de236798d5
Diego Neri Felix <Diego Neri>
2025-05-05 08:35:54 -0300

    Finaliza exercicio

--- Program.cs
+++ Program.cs
@@ -29,20 +29,49 @@ if (!(L2 >= 0 && L2 <= 10))
 Console.Write("Informe a nota da Lista 03:");
 L3 = Convert.ToDouble(Console.ReadLine());
 
+if (L3 < 0 || L3 > 10)
+{
+    Console.WriteLine("Nota da lista 03 inválida!");
+    return;
+}
 Console.Write("Informe a nota da Lista 04:");
 L4 = Convert.ToDouble(Console.ReadLine());
+
+if (L4 < 0 || L4 > 10)
+{
+    Console.WriteLine("Nota da lista 04 inválida!");
+    return;
+}
 
 Console.Write("Informe a nota da Lista 05:");
 L5 = Convert.ToDouble(Console.ReadLine());
 
+if (L5 < 0 || L5 > 10)
+{
+    Console.WriteLine("Nota da lista 05 inválida!");
+    return;
+}
+
 Console.Write("Informe a nota da Lista 06:");
 L6 = Convert.ToDouble(Console.ReadLine());
+
+if (L6 < 0 || L6 > 10)
+{
+    Console.WriteLine("Nota da lista 06 inválida!");
+    return;
+}
 
 Console.Write("Informe a nota da P1:");
 P1 = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Informe a nota da P2:");
 P2 = Convert.ToDouble(Console.ReadLine());
+
+if (P1 < 0 || P1 > 10 || P2 < 0 || P2 > 10)
+{
+   Console.WriteLine("P1 ou P2 com valor inválido");
+   return; 
+}
 
 mediaListas = (L1 + L2 + L3 + L4 + L5 + L6) / 6 * 0.2;
 mediaProvas = (P1 + P2) / 2 * 0.8;
@@ -60,6 +89,12 @@ else
     Console.Write("Informe a nota da P3:");
     P3 = Convert.ToDouble(Console.ReadLine());
     
+    if (P3 < 0 || P3 > 10)
+    {
+        Console.WriteLine("P3 com valor inválido!");
+        return;
+    }
+
     mediaProvas = (Math.Max(P1, P2) + P3) / 2 * 0.8;
     mediaFinal = mediaListas + mediaProvas;
 
